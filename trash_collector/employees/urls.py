043240b6124fc6bb7fafd26b6eb9a38948e0509c
danielleleigh <danielle.leigh90@gmail.com>
2021-11-02 12:01:51 -0500

    created employee urls

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 app_name = "employees"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('new/', views.create, name="create"),
+    path('edit_profile/', views.edit_profile, name="edit_profile"),
 ]
