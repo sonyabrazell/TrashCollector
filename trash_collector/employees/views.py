14378229fb96a546aa3da73ab37a64d0d84b3fe6
danielleleigh <danielle.leigh90@gmail.com>
2021-11-02 12:02:24 -0500

    added edit function for employees on views

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -4,7 +4,9 @@ from django.urls import reverse
 from django.shortcuts import render
 from django.apps import apps
 from .models import Employee
-from .models import Customer
+from django.core.exceptions import ObjectDoesNotExist
+from datetime import date
+
 
 # Create your views here.
 
@@ -13,8 +15,21 @@ from .models import Customer
 @login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Employee = apps.get_model('employees.Employee')
-    return render(request, 'employees/index.html')
+    Customer = apps.get_model('customers.Customer')
+    logged_in_user = request.user
+    try:
+        # This line will return the customer record of the logged-in user if one exists
+        logged_in_employee = Employee.objects.get(user=logged_in_user)
+
+        today = date.today()
+        
+        context = {
+            'logged_in_employee': logged_in_employee,
+            'today': today
+        }
+        return render(request, 'employees/index.html')
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('employees:create'))
 
 @login_required
 def create(request):
@@ -28,3 +43,25 @@ def create(request):
         return HttpResponseRedirect(reverse('employees:index'))
     else:
         return render(request, 'employees/create.html')
+    
+#edit function to edit an employee profile
+@login_required
+def edit_profile(request):
+    logged_in_user = request.user
+    logged_in_employee = Employee.objects.get(user=logged_in_user)
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        logged_in_employee.name = name_from_form
+        logged_in_employee.address = address_from_form
+        logged_in_employee.zip_code = zip_from_form
+        logged_in_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        context = {
+            'logged_in_employee': logged_in_employee
+        }
+        return render(request, 'employees/edit_profile.html', context)
+
+#confirm function to confirm pickups to have a charge of $20 to customer account. Also to confirm pickup.
