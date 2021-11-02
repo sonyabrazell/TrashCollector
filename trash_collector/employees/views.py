6c618a015a5d79a268726f475a1f6eceec9202ce
danielleleigh <danielle.leigh90@gmail.com>
2021-11-02 10:30:43 -0500

    created create function for employee

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,13 +1,30 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.apps import apps
+from .models import Employee
+from .models import Customer
 
 # Create your views here.
 
 # TODO: Create a function for each path created in employees/urls.py. Each will need a template as well.
 
-
+@login_required
 def index(request):
     # This line will get the Customer model from the other app, it can now be used to query the db for Customers
-    Customer = apps.get_model('customers.Customer')
+    Employee = apps.get_model('employees.Employee')
     return render(request, 'employees/index.html')
+
+@login_required
+def create(request):
+    logged_in_user = request.user
+    if request.method == "POST":
+        name_from_form = request.POST.get('name')
+        address_from_form = request.POST.get('address')
+        zip_from_form = request.POST.get('zip_code')
+        new_employee = Employee(name=name_from_form, user=logged_in_user, address=address_from_form, zip_code=zip_from_form)
+        new_employee.save()
+        return HttpResponseRedirect(reverse('employees:index'))
+    else:
+        return render(request, 'employees/create.html')
